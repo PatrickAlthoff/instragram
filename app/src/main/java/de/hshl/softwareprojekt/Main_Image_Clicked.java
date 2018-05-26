feaed305f90baaaf3b08b5cbe3dd02584910c73a
MSommer95 <maximilian.sommer@stud.hshl.de>
2018-05-26 23:55:13 +0200

    Back Button zur Toolbar in Vollansicht hinzugefügt.

--- app/src/main/java/de/hshl/softwareprojekt/Main_Image_Clicked.java
+++ app/src/main/java/de/hshl/softwareprojekt/Main_Image_Clicked.java
@@ -2,11 +2,14 @@ package de.hshl.softwareprojekt;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -23,10 +26,7 @@ public class Main_Image_Clicked extends AppCompatActivity implements View.OnClic
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main__image__clicked);
-
-
         Intent getImage = getIntent();
-
         this.kommentar = findViewById(R.id.clickedKomment);
         this.editKomm = findViewById(R.id.editKomment);
         this.clickedImage = findViewById(R.id.clickedImage);
@@ -35,18 +35,28 @@ public class Main_Image_Clicked extends AppCompatActivity implements View.OnClic
 
         Toolbar toolbar = findViewById(R.id.toolbar2);
         setSupportActionBar(toolbar);
-
-
         this.kommentar.setOnClickListener(this);
 
-
-
-
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 
     @Override
     public void onClick(View v) {
         this.editKomm.setVisibility(View.VISIBLE);
         this.editKomm.requestFocus();
+        this.editKomm.setText("");
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handle arrow click here
+        if (item.getItemId() == android.R.id.home) {
+            finish(); // close this activity and return to preview activity (if there is any)
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
