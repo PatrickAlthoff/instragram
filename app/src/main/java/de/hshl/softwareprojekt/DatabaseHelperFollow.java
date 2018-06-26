d21e0a41f48ea12e8109b7088b5ed6d1470863c8
pfuetze <Kea.Pfuetzner@stud.hshl.de>
2018-06-26 13:06:31 +0200

    Datenbank für die Follow Funktion

--- app/src/main/java/de/hshl/softwareprojekt/DatabaseHelperFollow.java
+++ app/src/main/java/de/hshl/softwareprojekt/DatabaseHelperFollow.java
@@ -2,9 +2,14 @@ package de.hshl.softwareprojekt;
 
 import android.content.ContentValues;
 import android.content.Context;
+
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import static android.content.ContentValues.TAG;
 
 public class DatabaseHelperFollow  extends SQLiteOpenHelper {
 
@@ -12,13 +17,13 @@ public class DatabaseHelperFollow  extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
     public static final String TABLE_NAME = "Follow";
     public static final String COLUMN_ID = "_id";
-    public static final String COLUMN_is= "isFollowing";
+    public static final String COLUMN_is = "isFollowing";
     public static final String COLUMN_by = "followedBy";
 
     public DatabaseHelperFollow(Context context) {
         super(context, DATABASE_NAME , null, DATABASE_VERSION);
     }
-//
+
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE " + TABLE_NAME + "(" +
@@ -33,39 +38,82 @@ public class DatabaseHelperFollow  extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
     }
-    public boolean insertData(String id, int isFollowing, int followedBy) {
-        SQLiteDatabase db = getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(COLUMN_ID, id);
-        contentValues.put(COLUMN_is, isFollowing);
-        contentValues.put(COLUMN_by, followedBy);
-        db.insert(TABLE_NAME, null, contentValues);
-        return true;
+    public void insertData(String id, int isFollowing, int followedBy) {
+        long rowId = -1;
+        SQLiteDatabase db = null;
+        try {
+            db = getWritableDatabase();
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(COLUMN_ID, id);
+            contentValues.put(COLUMN_is, isFollowing);
+            contentValues.put(COLUMN_by, followedBy);
+            db.insert(TABLE_NAME, null, contentValues);
+
+        } catch (SQLiteException exception) {
+            Log.e(TAG, "insertData()" + exception);
+        } finally {
+            Log.d(TAG, "InsertData()" + rowId + ")");
+            if (db != null) {
+                db.close();
+            }
+        }
     }
-    public boolean updateData(Integer id, int isFollowing, int FollowedBy) {
-        SQLiteDatabase db = this.getWritableDatabase();
+    public void updateData(Integer id, int isFollowing, int FollowedBy) {
+        int rowsUpdated = 0;
+        SQLiteDatabase db = null;
+        try {
+            db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put(COLUMN_ID, id);
         contentValues.put(COLUMN_is, isFollowing);
         contentValues.put(COLUMN_by, FollowedBy);
         db.update(TABLE_NAME, contentValues, COLUMN_ID + " = ? ", new String[] { Integer.toString(id) } );
-        return true;
+
+        rowsUpdated = db.update(TABLE_NAME, contentValues,"id= "+ COLUMN_ID,null);
+        Log.d(TAG, "updateData()affected"+ rowsUpdated + "rows");
+         } catch(SQLiteException exception){
+            Log.e(TAG, "updateData()"+ exception);
+        } finally {
+            if (db != null){
+                db.close();
+            }
+        }
     }
-    public Cursor getPerson(int id) {
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor res = db.rawQuery( "SELECT * FROM " + TABLE_NAME + " WHERE " +
-                COLUMN_ID + "=?", new String[] { Integer.toString(id) } );
-        return res;
+    public void deleteData(int id){
+        int rowsDeleted = 0;
+        SQLiteDatabase db = null;
+
+        try{
+            db = getWritableDatabase();
+            rowsDeleted = db.delete(TABLE_NAME, "id=" + id, null);
+            Log.d(TAG, "deleteData() affected" + rowsDeleted + "rows");
+        }catch(SQLiteException exception){
+            Log.e(TAG, "deleteData()"+ exception);
+        } finally {
+            if (db !=null){
+                db.close();
+            }
+        }
+
     }
-    public Cursor getAllPersons() {
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor res = db.rawQuery( "SELECT * FROM " + TABLE_NAME, null );
-        return res;
+    public void setIsFollowing(){
+        SQLiteDatabase db=this.getWritableDatabase();
+        db.execSQL("update Follow set isFollowing = _id ");
     }
-    public Integer deletePerson(Integer id) {
+    public void setIsFollowedBy(){
+        SQLiteDatabase db=this.getWritableDatabase();
+        db.execSQL("update Follow set FollowedBy = _id ");
+    }
+    public Cursor getFollowingData(){
         SQLiteDatabase db = this.getWritableDatabase();
-        return db.delete(TABLE_NAME,
-                COLUMN_ID + " = ? ",
-                new String[] { Integer.toString(id) });
+         String query = "SELECT * FROM " + COLUMN_is ;    // query nicht korrekt
+         Cursor data = db.rawQuery(query, null);
+        return data;
+    }
+    public Cursor getFollowerData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+       String query = "SELECT * FROM " + COLUMN_is ;    // query nicht korrekt
+        Cursor data = db.rawQuery(query, null);
+        return data;
     }
 }
